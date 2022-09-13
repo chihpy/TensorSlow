d4f8342814e3f34c1f0b0b45e39e384dbe83ca29
poyuanchih <poyuanchih@gmail.com>
2022-09-14 01:06:24 +0800

    feat: plot_dot_graph

---
+++ steps/step26_graph_viz.py
@@ -0,0 +1,36 @@
+
+if "__file__" in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import numpy as np
+from tensorslow import Variable
+from tensorslow.utils import plot_dot_graph
+
+def goldstein(x, y):
+    z = (1 + (x + y + 1) ** 2 * (19 - 14 * x + 3 * x ** 2 - 14 * y + 6 * x * y + 3 * y ** 2)) *\
+        (30 + (2 * x - 3 * y)**2 * (18 - 32 * x + 12 * x ** 2 + 48 * y - 36 * x * y + 27 * y ** 2))
+    return z
+
+#x0 = Variable(np.array(1.0))
+#x1 = Variable(np.array(1.0))
+#y = x0 + x1
+#
+#x0.name = 'x0'
+#x1.name = 'x1'
+#y.name = 'y'
+#
+#txt = get_dot_graph(y, verbose=False)
+#print(txt)
+#
+#with open('sample.dot', 'w') as o:
+#    o.write(txt)
+x = Variable(np.array(1.0))
+y = Variable(np.array(1.0))
+z = goldstein(x, y)
+z.backward()
+
+x.name = 'x'
+y.name = 'y'
+z.name = 'z'
+plot_dot_graph(z, verbose=False, to_file='../figures/goldstein.png')
---
+++ tensorslow/utils.py
@@ -0,0 +1,84 @@
+import os
+import subprocess
+
+if __name__ == "__main__":
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from tensorslow import Variable
+import numpy as np
+
+def _dot_var(v, verbose=False):
+    dot_var = '{} [label="{}", color=orange, style=filled]\n'
+
+    name = '' if v.name is None else v.name
+    if verbose and v.data is not None:
+        if v.name is not None:
+            name += ': '
+        name += str(v.shape) + ' ' + str(v.dtype)
+    return dot_var.format(id(v), name)
+
+def _dot_func(f):
+    dot_func = '{} [label="{}", color=lightblue, style=filled, shape=box]\n'
+    txt = dot_func.format(id(f), f.__class__.__name__)
+
+    dot_edge = '{} -> {}\n'
+    for x in f.inputs:
+        txt += dot_edge.format(id(x), id(f))
+    
+    for y in f.outputs:
+        txt += dot_edge.format(id(f), id(y()))  # note: y is weakref
+    return txt
+
+def get_dot_graph(output, verbose=True):
+    txt = ''
+    funcs = []
+    seen_set = set()
+
+    def add_func(f):
+        if f not in seen_set:
+            funcs.append(f)
+            # funcs.sort(key=lambda x: x.generation)
+            seen_set.add(f)
+    add_func(output.creator)
+    txt += _dot_var(output, verbose)
+
+    while funcs:
+        func = funcs.pop()
+        txt += _dot_func(func)
+        for x in func.inputs:
+            txt += _dot_var(x, verbose)
+
+            if x.creator is not None:
+                add_func(x.creator)
+    return 'digraph g{\n' + txt +'}'
+
+def plot_dot_graph(output, verbose=True, to_file='graph.png'):
+    dot_graph = get_dot_graph(output, verbose)
+
+    tmp_dir = os.path.join(os.path.expanduser('~'), '.tensorslow')
+    if not os.path.exists(tmp_dir):
+        os.mkdir(tmp_dir)
+    graph_path = os.path.join(tmp_dir, 'tmp_graph.dot')
+
+    with open(graph_path, 'w') as f:
+        f.write(dot_graph)
+    
+    extension = os.path.splitext(to_file)[1][1:]  # png
+    cmd = 'dot {} -T {} -o {}'.format(graph_path, extension, to_file)
+    subprocess.run(cmd, shell=True)
+
+
+if __name__ == "__main__":
+    # _dot_var usage
+    print("-"*20 + '_dot_var')
+    x = Variable(np.random.randn(2, 3))
+    x.name = 'x'
+    print(_dot_var(x))
+    print(_dot_var(x, verbose=True))
+    # _dot_func usage
+    print("-"*20 + '_dot_func')
+    x0 = Variable(np.array(1.0))
+    x1 = Variable(np.array(1.0))
+    y = x0 + x1
+    txt = _dot_func(y.creator)
+    print(txt)
